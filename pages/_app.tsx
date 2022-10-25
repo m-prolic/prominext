ba69e8690eb50f852b328fed0e90cb64f9ae1577
Milos Prolic <milos.prolic1@howdengroup.com>
2022-10-25 15:21:25 +0200

    Tailwind initialization and test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../public/fonts/fonts.css';
+import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1 style={{ fontFamily: 'Arvo', fontWeight: 'bold' }}>
-      hi there, THIS is only test for FONTS
-    </h1>
-  );
+  return <h1 className="underline">Hello world, this is Tailwind CSS</h1>;
 };
 
 export default Home;
